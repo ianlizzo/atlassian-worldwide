a51498b8e5ee40b3900fe7955536674a73ada432
ilizarda <ilizarda@atlassian.com>
2019-06-27 19:37:34 -0700

    Updates

---
+++ precache-manifest.ee75b8076041802ca1dda24b75a01249.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b91d75b99930f1c81557c2e921c4bf4",
+    "url": "/atlassian-worldwide/index.html"
+  },
+  {
+    "revision": "133763abb6e7759bb395",
+    "url": "/atlassian-worldwide/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "9cc3c5303d02e2be7b09",
+    "url": "/atlassian-worldwide/static/css/main.88dd9740.chunk.css"
+  },
+  {
+    "revision": "133763abb6e7759bb395",
+    "url": "/atlassian-worldwide/static/js/2.f7f43112.chunk.js"
+  },
+  {
+    "revision": "9cc3c5303d02e2be7b09",
+    "url": "/atlassian-worldwide/static/js/main.f65348ea.chunk.js"
+  },
+  {
+    "revision": "b4ec6ae4ed003563f9ad",
+    "url": "/atlassian-worldwide/static/js/runtime~main.7d1a1058.js"
+  },
+  {
+    "revision": "3e88f790b9487abe3a1ee5f23d063466",
+    "url": "/atlassian-worldwide/static/media/AMSTERDAM.3e88f790.png"
+  },
+  {
+    "revision": "3ce8205d13e88efc38ff1f08d9fb4ceb",
+    "url": "/atlassian-worldwide/static/media/AUSTIN.3ce8205d.png"
+  },
+  {
+    "revision": "f94c6a7c9b42edfd54c0b07386cbc634",
+    "url": "/atlassian-worldwide/static/media/BANGALORE.f94c6a7c.png"
+  },
+  {
+    "revision": "9017af52cc3ce51db080cdeeb5e7699d",
+    "url": "/atlassian-worldwide/static/media/MANILA.9017af52.png"
+  },
+  {
+    "revision": "ec96459ff6aa8712f64f81643dc12098",
+    "url": "/atlassian-worldwide/static/media/MTV.ec96459f.png"
+  },
+  {
+    "revision": "41c024ef100e7f4db97a2ebb2a9af90e",
+    "url": "/atlassian-worldwide/static/media/NYC.41c024ef.png"
+  },
+  {
+    "revision": "74e665a61c3a594716bd8759449aad72",
+    "url": "/atlassian-worldwide/static/media/SF.74e665a6.png"
+  },
+  {
+    "revision": "4d8da4e5682cec70b874d4feffc202a8",
+    "url": "/atlassian-worldwide/static/media/Sydney.4d8da4e5.png"
+  },
+  {
+    "revision": "0469007628c4512acac786fb2725dfd8",
+    "url": "/atlassian-worldwide/static/media/YOKOHAMA.04690076.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/atlassian-worldwide/static/media/logo.5d5d9eef.svg"
+  }
+]);
